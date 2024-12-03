326567ac231813a10b38c5dcd6949d0da29ee598
AZAD021997 <tchagnaobaidha@gmail.com>
2024-12-02 21:42:16 -0500

    Add files via upload

--- SDEV140_Final_Project/translatorwithgui.py
+++ SDEV140_Final_Project/translatorwithgui.py
@@ -1,1 +1,14 @@
-Communication Multi Languages Translator
+# FILE:translatorwithgui.py
+# DATE:2024-12-02
+# AUTHOR:Azad Baidha Tchagnaou
+# DESCRIPTION: """This program will translate from one language to another. SDEV 140's final project."
+
+from tkinter import *
+from tkinter import ttk, messagebox
+import googletrans
+from googletrans import Translator
+
+root=Tk()
+root.title("Communication Multi Languages Translator")
+root.geometry("1100x500")
+root.resizable(False,False)
